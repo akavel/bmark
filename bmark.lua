32e46a07173dc217693e34c1217bdbcdd61fab00
Mateusz Czapliński <czapkofan@gmail.com>
2017-02-25 21:02:15 +0100

    add function dragDropGetData

--- bmark.lua
+++ bmark.lua
@@ -13,8 +13,6 @@ local winapi = require 'winapi'
 -- require 'winapi.monitor'
 require 'winapi.windowclass'
 -- require 'winapi.ole'  -- TODO: remove, should not be needed because included via winapi.dragdrop
-require 'winapi.clipboard'
-require 'winapi.memory'
 require 'dragdrop'
 
 -- -- info about the monitor which currently has mouse cursor
@@ -26,61 +24,28 @@ local win = winapi.Window{
 }
 
 -- Work based on http://www.catch22.net/tuts/drop-target and https://www.codeproject.com/Articles/13601/COM-in-plain-C
-dragenter = function(pDataObj, grfKeyState, pt, pdwEffect)
-	print 'DragEnter!'
-	local formats = dragDropFormats(pDataObj)
-	local htmlFormat = formats['HTML Format']
-	if not htmlFormat then
-		return
+local drop_target = winapi.simpleDropTarget{
+	DragEnter = function(pDataObj, grfKeyState, pt, pdwEffect)
+		print 'DragEnter!'
+		local formats = dragDropFormats(pDataObj)
+		local htmlFormat = formats['HTML Format']
+		if not htmlFormat then
+			return winapi.E_UNEXPECTED
+		end
+		local f = htmlFormat
+		if f.dwAspect==1 and f.tymed==1 then
+			-- print('good')
+			print(dragDropGetData(pDataObj, f))
+		else
+			print(('got "HTML Format", but unexpected dwAspect=%d and tymed=%d'):format(
+				f.dwAspect, f.tymed))
+		end
+		return winapi.E_UNEXPECTED
 	end
-	local f = htmlFormat
-	if f.dwAspect==1 and f.tymed==1 then
-		local medium = ffi.new 'STGMEDIUM'
-		winapi.checkz(pDataObj:GetData(f, medium))
-		print(stgMediumData(medium))
-		winapi.ReleaseStgMedium(medium)
-	else
-		print(('got "HTML Format", but unexpected dwAspect=%d and tymed=%d'):format(
-			f.dwAspect, f.tymed))
-	end
-	return winapi.E_UNEXPECTED
-end
-winapi.RegisterDragDrop(win.hwnd, winapi.simpleDropTarget{DragEnter = dragenter})
+}
+winapi.RegisterDragDrop(win.hwnd, drop_target)
 -- TODO: winapi.RevokeDragDrop()
-
-function stgMediumData(medium)
-	assert(ffi.istype('STGMEDIUM', medium))
-	assert(medium.tymed == winapi.TYMED_HGLOBAL, "expected TYMED_HGLOBAL, got "..tostring(medium.tymed))
-	local hglobal = medium.hGlobal
-	local p = winapi.GlobalLock(hglobal)
-	local sz = winapi.GlobalSize(hglobal)
-	-- TODO(akavel): conditionally call winapi.mbs instead?
-	local s = ffi.string(p, sz) -- TODO: cut at '\0' byte
-	winapi.GlobalUnlock(hglobal)
-	return s
-end
-function dragDropFormats(pDataObj)
-	local result = {}
-	local enum = ffi.new 'IEnumFORMATETC*[1]'
-	winapi.checkz(pDataObj:EnumFormatEtc(winapi.DATADIR_GET, enum))
-	for i = 1,100 do
-		local f = ffi.new 'FORMATETC'
-		f.ptd = nil  -- TODO(akavel): do we need this, or is it already 0 by default?
-		local next = enum[0]:Next(1, f, nil)
-		if next ~= winapi.S_OK then
-			break
-		end
-		if f.ptd ~= nil then
-			winapi.CoTaskMemFree(f.ptd)
-			f.ptd = nil
-		end
-		f.lindex = -1  -- make it easy to use the result immediately with pDataObj:GetData()
-		local name = winapi.CF_NAMES[f.cfFormat] or winapi.mbs(winapi.GetClipboardFormatName(f.cfFormat)) or ''
-		result[name] = f
-	end
-	enum[0]:Release()
-	return result
-end
 
 -- pass control to the GUI system & message loop
 os.exit(winapi.MessageLoop())
+
--- dragdrop.lua
+++ dragdrop.lua
@@ -1,3 +1,47 @@
 local ffi = require 'ffi'
 local winapi = require 'winapi'
+require 'winapi.clipboard'
+require 'winapi.memory'
 require 'winapi_dragdrop_fix'
+
+function dragDropFormats(pDataObj)
+	local result = {}
+	local enum = ffi.new 'IEnumFORMATETC*[1]'
+	winapi.checkz(pDataObj:EnumFormatEtc(winapi.DATADIR_GET, enum))
+	for i = 1,100 do
+		local f = ffi.new 'FORMATETC'
+		f.ptd = nil  -- TODO(akavel): do we need this, or is it already 0 by default?
+		local next = enum[0]:Next(1, f, nil)
+		if next ~= winapi.S_OK then
+			break
+		end
+		if f.ptd ~= nil then
+			winapi.CoTaskMemFree(f.ptd)
+			f.ptd = nil
+		end
+		f.lindex = -1  -- make it easy to use the result immediately with pDataObj:GetData()
+		local name = winapi.CF_NAMES[f.cfFormat] or winapi.mbs(winapi.GetClipboardFormatName(f.cfFormat)) or ''
+		result[name] = f
+	end
+	enum[0]:Release()
+	return result
+end
+-- NOTE: only TYMED_HGLOBAL supported as of now
+function dragDropGetData(pDataObj, formatEtc)
+	assert(ffi.istype('FORMATETC', formatEtc))
+	local medium = ffi.new 'STGMEDIUM'
+	winapi.checkz(pDataObj:GetData(formatEtc, medium))
+	if medium.tymed ~= winapi.TYMED_HGLOBAL then
+		winapi.ReleaseStgMedium(medium)
+		error('expected TYMED_HGLOBAL, got '..tostring(medium.tymed))
+	end
+	local hglobal = medium.hGlobal
+	local p = winapi.GlobalLock(hglobal)
+	local sz = winapi.GlobalSize(hglobal)
+	-- TODO(akavel): conditionally call winapi.mbs instead? or copy a blob of bytes?
+	local s = ffi.string(p, sz) -- TODO: cut at '\0' byte
+	winapi.GlobalUnlock(hglobal)
+	winapi.ReleaseStgMedium(medium)
+	return s
+end
+
